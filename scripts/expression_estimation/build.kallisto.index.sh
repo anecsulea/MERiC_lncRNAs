033ea2b64b13d300b4705908eb8bdced0dab7350
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-07 16:05:32 +0100

    kallisto index

---
+++ scripts/expression_estimation/build.kallisto.index.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export annot=$1
+export path=$2
+export pathTools=$3
+
+####################################################################################
+
+export pathResults=${path}/results/kallisto_indexes
+
+####################################################################################
+
+singularity -B ${path} ${pathTools}/kallisto.sif kallisto index -i ${pathResults}/${annot} ${pathResults}/${annot}.fa
+
+####################################################################################
