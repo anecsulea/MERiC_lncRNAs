6e0ed4f3d56dbcdeb18c8c44406d2a6184048e2a
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-07 16:03:59 +0100

    extract cDNA sequences

---
+++ scripts/expression_estimation/extract.cDNA.sequences.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export annot=$1
+
+###########################################################################
+
+export pathEnsembl=../../data/ensembl_annotations
+export pathGenome=../../data/genome_sequence
+export pathResults=../../results/kallisto_indexes
+export pathScripts=../../scripts/expression_estimation
+
+export release=109
+export version=hg38
+
+##############################################################
+
+if [ -e ${pathResults} ]; then
+    echo "output dir exists"
+else
+    mkdir -p ${pathResults}
+fi
+
+##############################################################
+
+if [ ${annot} = "AllTranscripts" ]; then
+    export pathGTF=${pathEnsembl}
+    export suffix=AllTranscripts_Ensembl${release}
+fi
+
+##############################################################
+
+perl ${pathScripts}/extract.cDNA.sequences.pl --pathAnnotGTF=${pathGTF}/${suffix}.gtf.gz --forbiddenChromo=MT --pathGeneInfo=${pathEnsembl}/GeneInfo_Ensembl${release}.txt --forbiddenBiotypes=Mt_rRNA,rRNA,rRNA_pseudogene --pathGenomeSequence=${pathGenome}/genome_ensembl${release}_${version}.fa.gz --pathOutput=${pathResults}/${suffix}_noMT_norRNA.fa
+
+###############################################################
