ec4a1a7feac06972414a33b77e5460ff833e4319
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-07 15:41:14 +0100

    download data from Ensembl

---
+++ scripts/get_ensembl_data/download.annotations.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+#####################################################################
+
+wget -O ../../data/ensembl_annotations/AllTranscripts_Ensembl109.gtf.gz https://ftp.ensembl.org/pub/release-109/gtf/homo_sapiens/Homo_sapiens.GRCh38.109.gtf.gz
+
+#####################################################################
---
+++ scripts/get_ensembl_data/download.genome.sequence.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+#####################################################################
+
+wget -O ../../data/genome_sequence/genome_ensembl109_hg38.fa.gz https://ftp.ensembl.org/pub/release-109/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+
+#####################################################################
