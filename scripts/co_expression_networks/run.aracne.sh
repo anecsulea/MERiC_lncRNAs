ab420e0c26a17f0b844c22cb4410ba3efbaf91ac
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-09 18:50:42 +0100

    aracne

---
+++ scripts/co_expression_networks/run.aracne.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export exp_dataset=$1
+export TF_dataset=$2
+export path=$3
+export pathTools=$4
+
+####################################################################################
+
+export pathResults=${path}/results/co_expression_networks/${exp_dataset}
+
+####################################################################################
+
+for i in {1..100}
+do
+    echo $i
+
+    java -Xmx5G -jar ${pathTools}/ARACNe-AP/dist/aracne.jar -e ${pathResults}/expression_data.txt -o ${pathResults}/${TF_dataset} --tfs ${pathResults}/${TF_dataset}.txt --pvalue 1E-8 --seed ${i} --threads=${threads}
+
+done
+
+####################################################################################
