3d5a5f762b13f1b92893fe343df1079d30ededa1
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-09 13:21:47 +0100

    start prepare sample info

---
+++ scripts/differential_expression/prepare.sample.info.R
@@ -0,0 +1,14 @@
+###########################################################################
+
+pathDocs="../../docs/"
+
+###########################################################################
+
+tumor.samples=read.table(paste(pathDocs, "TumorSamples_Ng2022.txt", sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"")
+
+###########################################################################
+
+liver.samples=read.table(paste(pathDocs, "LiverSamples_Ng2022.txt", sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"")
+
+###########################################################################
+
