b7fa25172fd7fde5a3a596bb94fc34683d96fe91
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-09 13:52:53 +0100

    discard haplotypes

--- scripts/expression_estimation/extract.cDNA.sequences.sh
+++ scripts/expression_estimation/extract.cDNA.sequences.sh
@@ -29,6 +29,15 @@ fi
 
 ##############################################################
 
-perl ${pathScripts}/extract.cDNA.sequences.pl --pathAnnotGTF=${pathGTF}/${suffix}.gtf.gz --forbiddenChromo=MT --pathGeneInfo=${pathEnsembl}/GeneInfo_Ensembl${release}.txt --forbiddenBiotypes=Mt_rRNA,rRNA,rRNA_pseudogene --pathGenomeSequence=${pathGenome}/genome_ensembl${release}_${version}.fa.gz --pathOutput=${pathResults}/${suffix}_noMT_norRNA.fa
+export haplo=""
+
+for chr in `cut -f 3 ${pathEnsembl}/GeneInfo_Ensembl${release}.txt | sort -u | grep CHR`
+do
+    export haplo=${chr},${haplo}
+done
+
+##############################################################
+
+perl ${pathScripts}/extract.cDNA.sequences.pl --pathAnnotGTF=${pathGTF}/${suffix}.gtf.gz --forbiddenChromo=MT,${haplo} --pathGeneInfo=${pathEnsembl}/GeneInfo_Ensembl${release}.txt --forbiddenBiotypes=Mt_rRNA,rRNA,rRNA_pseudogene --pathGenomeSequence=${pathGenome}/genome_ensembl${release}_${version}.fa.gz --pathOutput=${pathResults}/${suffix}_noMT_norRNA_nohaplo.fa
 
 ###############################################################
