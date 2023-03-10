a7079e7d2f8ac333ee7d1387fe5331c9fd035e6a
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-10 13:02:06 +0100

    aracne on the cluster

--- scripts/co_expression_networks/run.aracne.sh
+++ scripts/co_expression_networks/run.aracne.sh
@@ -5,10 +5,12 @@ export TF_dataset=$2
 export path=$3
 export pathTools=$4
 export threads=$5
+export scheduler=$6
 
 ####################################################################################
 
 export pathResults=${path}/results/co_expression_networks/${exp_dataset}
+export pathScripts=${path}/scripts/co_expression_networks
 
 ####################################################################################
 
@@ -16,7 +18,23 @@ for i in {1..100}
 do
     echo $i
 
-    java -Xmx5G -jar ${pathTools}/ARACNe-AP/dist/aracne.jar -e ${pathResults}/expression_data.txt -o ${pathResults}/${TF_dataset} --tfs ${pathResults}/${TF_dataset}.txt --pvalue 1E-8 --seed ${i} --threads=${threads}
+    echo "#!/bin/bash " > ${pathScripts}/bsub_script
+
+    if [ ${scheduler} = "slurm" ]; then
+	echo "#SBATCH --job-name=kallisto_${sample}" >>  ${pathScripts}/bsub_script
+	echo "#SBATCH --partition=normal" >>  ${pathScripts}/bsub_script
+	echo "#SBATCH --output=${pathScripts}/std_out_kallisto_${sample}" >>  ${pathScripts}/bsub_script
+	echo "#SBATCH --error=${pathScripts}/std_err_kallisto_${sample}" >>  ${pathScripts}/bsub_script
+	echo "#SBATCH --cpus-per-task=${threads}" >>  ${pathScripts}/bsub_script 
+	echo "#SBATCH --time=4:00:00" >>  ${pathScripts}/bsub_script 
+	echo "#SBATCH --mem=20G" >>  ${pathScripts}/bsub_script
+    fi
+    
+    echo "java -Xmx5G -jar ${pathTools}/ARACNe-AP/dist/aracne.jar -e ${pathResults}/expression_data.txt -o ${pathResults}/${TF_dataset} --tfs ${pathResults}/${TF_dataset}.txt --pvalue 1E-8 --seed ${i} --threads=${threads}"  >>  ${pathScripts}/bsub_script
+
+    if [ ${scheduler} = "slurm" ]; then
+	sbatch ${pathScripts}/bsub_script
+    fi
 
 done
 
