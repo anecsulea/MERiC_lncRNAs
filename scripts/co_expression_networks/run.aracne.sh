c2c3892420415c338ccddf238dbd76bbd828457e
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-10 16:45:01 +0100

    add seed as par in aracne

--- scripts/co_expression_networks/run.aracne.sh
+++ scripts/co_expression_networks/run.aracne.sh
@@ -6,6 +6,7 @@ export path=$3
 export pathTools=$4
 export threads=$5
 export scheduler=$6
+export seed=$7
 
 ####################################################################################
 
@@ -14,27 +15,24 @@ export pathScripts=${path}/scripts/co_expression_networks
 
 ####################################################################################
 
-for i in {1..100}
-do
-    echo $i
 
-    echo "#!/bin/bash " > ${pathScripts}/bsub_script
+echo "#!/bin/bash " > ${pathScripts}/bsub_script
 
-    if [ ${scheduler} = "slurm" ]; then
-	echo "#SBATCH --job-name=aracne_${i}" >>  ${pathScripts}/bsub_script
-	echo "#SBATCH --output=${pathScripts}/std_out_aracne_${i}" >>  ${pathScripts}/bsub_script
-	echo "#SBATCH --error=${pathScripts}/std_err_aracne_${i}" >>  ${pathScripts}/bsub_script
-	echo "#SBATCH --cpus-per-task=${threads}" >>  ${pathScripts}/bsub_script 
-	echo "#SBATCH --time=4:00:00" >>  ${pathScripts}/bsub_script 
-	echo "#SBATCH --mem=20G" >>  ${pathScripts}/bsub_script
-    fi
-    
-    echo "java -Xmx5G -jar ${pathTools}/ARACNe-AP/dist/aracne.jar -e ${pathResults}/expression_data.txt -o ${pathResults}/${TF_dataset} --tfs ${pathResults}/${TF_dataset}.txt --pvalue 1E-8 --seed ${i} --threads=${threads}"  >>  ${pathScripts}/bsub_script
+if [ ${scheduler} = "slurm" ]; then
+    echo "#SBATCH --job-name=aracne_${i}" >>  ${pathScripts}/bsub_script
+    echo "#SBATCH --output=${pathScripts}/std_out_aracne_${seed}" >>  ${pathScripts}/bsub_script
+    echo "#SBATCH --error=${pathScripts}/std_err_aracne_${seed}" >>  ${pathScripts}/bsub_script
+    echo "#SBATCH --cpus-per-task=${threads}" >>  ${pathScripts}/bsub_script 
+    echo "#SBATCH --time=4:00:00" >>  ${pathScripts}/bsub_script 
+    echo "#SBATCH --mem=20G" >>  ${pathScripts}/bsub_script
+fi
 
-    if [ ${scheduler} = "slurm" ]; then
-	sbatch ${pathScripts}/bsub_script
-    fi
+echo "java -Xmx5G -jar ${pathTools}/ARACNe-AP/dist/aracne.jar -e ${pathResults}/expression_data.txt -o ${pathResults}/${TF_dataset} --tfs ${pathResults}/${TF_dataset}.txt --pvalue 1E-8 --seed ${seed} --threads=${threads}"  >>  ${pathScripts}/bsub_script
 
-done
+if [ ${scheduler} = "slurm" ]; then
+    sbatch ${pathScripts}/bsub_script
+fi
+
+
 
 ####################################################################################
