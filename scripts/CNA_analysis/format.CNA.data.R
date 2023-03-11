4558cfacec5814c0df33f0f7f4eb8e07e897e884
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-11 16:24:56 +0100

    corrected method name

--- scripts/CNA_analysis/format.CNA.data.R
+++ scripts/CNA_analysis/format.CNA.data.R
@@ -27,6 +27,6 @@ colnames(allex.bed)=c("chr", "start", "end")
 allex.bed$id=paste(allex.bed$chr,":", allex.bed$start, "-", allex.bed$end,sep="")
 allex.bed$chr=paste("chr", allex.bed$chr,sep="")
 
-write.table(allex.bed, file=paste(pathResults, "AllExonsv6_ampdel_hg19.bed",sep=""), row.names=F, col.names=F, sep="\t", quote=F)
+write.table(allex.bed, file=paste(pathResults, "AllExonv6_ampdel_hg19.bed",sep=""), row.names=F, col.names=F, sep="\t", quote=F)
 
 ######################################################################
