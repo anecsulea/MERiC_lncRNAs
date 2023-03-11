4c9b256f65bc95deec387f4f1210a729f66d575f
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-11 11:45:06 +0100

    consolidate aracne

---
+++ scripts/co_expression_networks/consolidate.aracne.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export exp_dataset=$1
+export TF_dataset=$2
+export path=$3
+export pathTools=$4
+
+####################################################################################
+
+export pathResults=${path}/results/co_expression_networks/${exp_dataset}/${TF_dataset}
+
+####################################################################################
+
+java -Xmx5G -jar ${pathTools}/ARACNe-AP/dist/aracne.jar -o ${pathResults} --consolidate
+
+####################################################################################
