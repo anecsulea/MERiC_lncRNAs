c355db51bc560eb68322152e0381701373a95068
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-11 14:55:03 +0100

    changed file name

--- scripts/CNA_analysis/liftOver.CNA.data.sh
+++ scripts/CNA_analysis/liftOver.CNA.data.sh
@@ -9,7 +9,7 @@ pathResults=../../results/CNA_analysis
 
 for dataset in CRE AllExonsv6
 do
-    liftOver ${pathResults}/${dataset}_hg19.bed ${pathLiftOver}/hg19ToHg38.over.chain.gz  ${pathResults}/${dataset}_hg38.bed ${pathResults}/${dataset}_hg38.unmapped
+    liftOver ${pathResults}/${dataset}_ampdel_hg19.bed ${pathLiftOver}/hg19ToHg38.over.chain.gz  ${pathResults}/${dataset}_ampdel_hg38.bed ${pathResults}/${dataset}_ampdel_hg38.unmapped
 done
 
 ######################################################################
