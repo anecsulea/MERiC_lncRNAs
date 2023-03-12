438d7e2f54bb9dd3ccec91672657e285d98eab08
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-12 10:38:42 +0100

    pseudo in pca

--- scripts/differential_expression/pca.tissue.types.R
+++ scripts/differential_expression/pca.tissue.types.R
@@ -3,8 +3,10 @@
 pathExpression="../../results/expression_estimation/"
 pathDifferentialExpression="../../results/differential_expression/"
 pathAnnot="../../data/ensembl_annotations/"
+pathSampleInfo="../../results/sample_info/"
+pathResults="../../results/figures/analyses/"
 
-annot="AllTranscripts_Ensembl109_noMT_norRNA"
+annot="AllTranscripts_Ensembl109_noMT_norRNA_nohaplo"
 
 ########################################################################
 
@@ -16,7 +18,7 @@ tpm=read.table(paste(pathExpression, annot, "/AllSamples_KallistoRawTPM.txt",sep
 
 ########################################################################
 
-sampleinfo=read.table(paste(pathDifferentialExpression, "SampleInfo.txt", sep=""), h=T, stringsAsFactors=F,sep="\t")
+sampleinfo=read.table(paste(pathDifferentialExpression, "AnalyzedSamples.txt", sep=""), h=T, stringsAsFactors=F,sep="\t")
 
 col.samples=rep("gray40", nrow(sampleinfo))
 col.samples[which(sampleinfo$TissueType=="Tumor" & sampleinfo$EdmondsonGrade==1)]="yellow"
@@ -34,11 +36,15 @@ pc=geneinfo$Gene.stable.ID[which(geneinfo$Gene.type=="protein_coding")]
 
 lnc=geneinfo$Gene.stable.ID[which(geneinfo$Gene.type=="lncRNA")]
 
+pseudo=geneinfo$Gene.stable.ID[which(geneinfo$Gene.type%in%c("transcribed_unitary_pseudogene", "transcribed_unprocessed_pseudogene", "unitary_pseudogene", "unprocessed_pseudogene"))]
+
 ########################################################################
 
 tpm.pc=tpm[intersect(pc, rownames(tpm)),]
 
 tpm.lnc=tpm[intersect(lnc, rownames(tpm)),]
+
+tpm.pseudo=tpm[intersect(pseudo, rownames(tpm)),]
 
 ########################################################################
 
@@ -46,12 +52,20 @@ pca.pc=dudi.pca(t(log2(tpm.pc+1)), scale=F, center=T, scannf=F, nf=5)
 
 pca.lnc=dudi.pca(t(log2(tpm.lnc+1)), scale=F, center=T, scannf=F, nf=5)
 
+pca.pseudo=dudi.pca(t(log2(tpm.pseudo+1)), scale=F, center=T, scannf=F, nf=5)
+
 ########################################################################
 
-par(mfrow=c(1,2))
+pdf(file=paste(pathResults, "PCA_gene_types.pdf",sep=""),width=9,height=5)
+
+par(mfrow=c(1,3))
+par(mar=c(4.1,2.1,1.1,1.1))
 
 plot(pca.pc$li[,1], pca.pc$li[,2], pch=20, col=col.samples[rownames(pca.pc$li)])
 
 plot(pca.lnc$li[,1], pca.lnc$li[,2], pch=20, col=col.samples[rownames(pca.lnc$li)])
+
+plot(pca.pseudo$li[,1], pca.pseudo$li[,2], pch=20, col=col.samples[rownames(pca.pseudo$li)])
+dev.off()
 
 ########################################################################
