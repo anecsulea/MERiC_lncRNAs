aa64a053a43213e7a60dbef9b0bc145551afb2c7
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-03-14 18:40:50 +0100

    starting fig1

--- scripts/main_figures/figure1.R
+++ scripts/main_figures/figure1.R
@@ -10,6 +10,8 @@ annot="AllTranscripts_Ensembl109_noMT_norRNA_nohaplo"
 
 set.seed(19)
 
+library(ade4)
+
 ########################################################################
 
 if(prepare){
@@ -54,22 +56,59 @@ if(prepare){
   
   pca.pseudo=dudi.pca(t(log2(tpm.pseudo+1)), scale=F, center=T, scannf=F, nf=5)
 
+  explained.pc=round(100*pca.pc$eig/sum(pca.pc$eig))
+
+  explained.lnc=round(100*pca.lnc$eig/sum(pca.lnc$eig))
   
+  explained.pseudo=round(100*pca.pseudo$eig/sum(pca.pseudo$eig))
+  
+
+  ########################################################################
+
+  prepare=F
 }
 
 ########################################################################
 #######################################################################
 
-pdf(file=paste(pathResults, "PCA_gene_types.pdf",sep=""),width=9,height=5)
+pdf(file=paste(pathResults, "Figure1.pdf",sep=""),width=10,height=4)
 
-p
-par(mar=c(4.1,2.1,1.1,1.1))
+m=matrix(c(rep(NA,10*9)), nrow=10)
 
-plot(pca.pc$li[,1], pca.pc$li[,2], pch=20, col=col.samples[rownames(pca.pc$li)])
+for(i in 1:10){
+  m[i,]=c(rep(1, 3), rep(2, 3), rep(3, 3))
+}
+layout(m)
 
-plot(pca.lnc$li[,1], pca.lnc$li[,2], pch=20, col=col.samples[rownames(pca.lnc$li)])
+par(mar=c(4.1,4.1,2.1,1.1))
 
-plot(pca.pseudo$li[,1], pca.pseudo$li[,2], pch=20, col=col.samples[rownames(pca.pseudo$li)])
+plot(-pca.pc$li[,1], pca.pc$li[,2], pch=20, col=col.samples[rownames(pca.pc$li)], xlab="", ylab="", axes=F, cex=1.25)
+axis(side=1, mgp=c(3, 0.75, 0), cex.axis=1.2)
+axis(side=2, mgp=c(3, 0.5, 0), cex.axis=1.2)
+box()
+mtext("protein-coding genes", side=3, line=0.25)
+mtext(paste("PC1 (", explained.pc[1],"% variance)",sep=""), side=1, line=2.25)
+mtext(paste("PC2 (", explained.pc[2],"% variance)",sep=""), side=2, line=2.25)
+
+plot(pca.lnc$li[,1], pca.lnc$li[,2], pch=20, col=col.samples[rownames(pca.lnc$li)], ylab="", xlab="",axes=F, cex=1.25)
+axis(side=1, mgp=c(3, 0.75, 0), cex.axis=1.2)
+axis(side=2, mgp=c(3, 0.5, 0), cex.axis=1.2)
+box()
+mtext("lncRNAs", side=3, line=0.25)
+mtext(paste("PC1 (", explained.lnc[1],"% variance)",sep=""), side=1, line=2.25)
+mtext(paste("PC2 (", explained.lnc[2],"% variance)",sep=""), side=2, line=2.25)
+
+
+plot(pca.pseudo$li[,1], -pca.pseudo$li[,2], pch=20, col=col.samples[rownames(pca.pseudo$li)], ylab="", xlab="", axes=F, cex=1.25)
+axis(side=1, mgp=c(3, 0.75, 0), cex.axis=1.2)
+axis(side=2, mgp=c(3, 0.5, 0), cex.axis=1.2)
+box()
+mtext("pseudogenes", side=3, line=0.25)
+mtext(paste("PC1 (", explained.pseudo[1],"% variance)",sep=""), side=1, line=2.25)
+mtext(paste("PC2 (", explained.pseudo[2],"% variance)",sep=""), side=2, line=2.25)
+
+
+
 dev.off()
 
 ########################################################################
