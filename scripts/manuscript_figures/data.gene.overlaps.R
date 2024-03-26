496575fc085020699d4f6f057b65fa5cfac5e5da
Anamaria Nesulea <anamaria.necsulea@univ-lyon1.fr>
2024-03-26 20:11:27 +0100

    gene overlaps

---
+++ scripts/manuscript_figures/data.gene.overlaps.R
@@ -0,0 +1,25 @@
+##########################################################################
+
+pathOverlaps="../../results/gene_overlaps/"
+pathRData="../../data_for_publication/RData/"
+
+annot="AllTranscripts_Ensembl109"
+
+##########################################################################
+
+## genic overlaps
+gene.overlaps=read.table(paste(pathOverlaps, "OverlappingGenes_", annot,".txt",sep=""), h=T, stringsAsFactors=F)
+
+## bidirectional promoters 1kb
+
+biprom1kb=read.table(paste(pathOverlaps, "DistanceAntisenseTSS_MaxDist1kb_AllTranscripts_Ensembl109.txt", sep=""), h=T, stringsAsFactors=F)
+biprom1kb=biprom1kb[, c("GeneID", "GenesCloseTSS")]
+
+biprom5kb=read.table(paste(pathOverlaps, "DistanceAntisenseTSS_MaxDist5kb_AllTranscripts_Ensembl109.txt", sep=""), h=T, stringsAsFactors=F)
+biprom5kb=biprom5kb[, c("GeneID", "GenesCloseTSS")]
+
+##########################################################################
+
+save(list=c("gene.overlaps", "biprom1kb", "biprom5kb"), file=paste(pathRData, "data.gene.overlaps.RData",sep=""))
+
+##########################################################################
