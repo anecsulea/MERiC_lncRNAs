38a2c424e7ec079199e8ee5243bd7eb2d9b28595
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2024-03-26 15:35:05 +0100

    data diff exp

--- scripts/manuscript_figures/data.diffexp.R
+++ scripts/manuscript_figures/data.diffexp.R
@@ -7,9 +7,9 @@ annot="AllTranscripts_Ensembl109_noMT_norRNA_nohaplo"
 
 ##########################################################################
 
-diffexp.grades=read.table(paste(pathDifferentialExpression, "DifferentialExpression_EdmondsonGrade_34_vs_12.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
+diffexp.grades=read.table(paste(pathDifferentialExpression, annot, "/DifferentialExpression_EdmondsonGrade_34_vs_12.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
 
-diffexp.tissues=read.table(paste(pathDifferentialExpression, "DifferentialExpression_Tumor_vs_Liver.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
+diffexp.tissues=read.table(paste(pathDifferentialExpression, annot, "/DifferentialExpression_Tumor_vs_Liver.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
 
 ##########################################################################
 
