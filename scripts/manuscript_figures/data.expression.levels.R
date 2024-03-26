45194ac95b34b8c01886e3ba6624a132c6736b0a
Anamaria Nesulea <anamaria.necsulea@univ-lyon1.fr>
2024-03-26 19:56:54 +0100

    exp levels

--- scripts/manuscript_figures/data.expression.levels.R
+++ scripts/manuscript_figures/data.expression.levels.R
@@ -1,6 +1,7 @@
 ##########################################################################
 
 pathExpression="../../results/expression_estimation/"
+pathExpressionTCGA="../../results/expression_estimation_TCGA/"
 pathRData="../../data_for_publication/RData/"
 
 annot="AllTranscripts_Ensembl109_noMT_norRNA_nohaplo"
@@ -11,6 +12,14 @@ tpm=read.table(paste(pathExpression, annot, "/AllSamples_KallistoRawTPM.txt",sep
 
 ##########################################################################
 
-save(tpm, file=paste(pathRData, "data.expression.levels.RData",sep=""))
+save(tpm, file=paste(pathRData, "data.expression.levels.MERiC.RData",sep=""))
+
+##########################################################################
+
+tpm=read.table(paste(pathExpressionTCGA, annot, "/AllSamples_KallistoRawTPM.txt",sep=""), h=T, stringsAsFactors=F, sep="\t")
+
+##########################################################################
+
+save(tpm, file=paste(pathRData, "data.expression.levels.TCGA.RData",sep=""))
 
 ##########################################################################
