218843f145f5b973ff401ae516be34b48f816e17
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2024-03-26 17:06:24 +0100

    rerun all

---
+++ scripts/manuscript_figures/rerun.all.data.R
@@ -0,0 +1,20 @@
+############################################################
+
+objects=ls()
+
+files=c("data.gene.info.R", "data.PubMed.analysis.R",  "data.diffexp.R",  "data.expression.levels.R", "data.sequence.conservation.R")
+
+############################################################
+
+for(file in files){
+
+    clear=setdiff(ls(), c("objects", "file", "files"))
+
+    rm(list=clear)
+
+    print(file)
+
+    source(file)
+}
+
+############################################################
