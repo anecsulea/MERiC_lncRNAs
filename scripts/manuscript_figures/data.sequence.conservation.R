1ddc2ed2afe121e8304520f7e89e5a5ee6d07898
Anamaria Nesulea <anamaria.necsulea@univ-lyon1.fr>
2024-03-26 19:54:07 +0100

    phast

--- scripts/manuscript_figures/data.sequence.conservation.R
+++ scripts/manuscript_figures/data.sequence.conservation.R
@@ -5,8 +5,8 @@ pathRData="../../data_for_publication/RData/"
 
 ##########################################################################
 
-for(phast in "30way", "100way"){
-
+for(phast in c("30way", "100way")){
+    
     phastcons=read.table(paste(pathConservation, phast,"/PhastCons_GeneAverage_EnsemblNoOverlaps.txt", sep=""), h=T, stringsAsFactors=F)
 
     rownames(phastcons)=phastcons$Gene
