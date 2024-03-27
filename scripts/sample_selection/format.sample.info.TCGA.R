1be301be2620596cd684958461c04938dd44ee0e
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2024-03-27 06:45:52 +0100

    sample info TCGA

---
+++ scripts/sample_selection/format.sample.info.TCGA.R
@@ -0,0 +1,22 @@
+###########################################################################
+
+pathTCGA="../../data/TCGA/"
+pathRData="../../data_for_publication/RData/"
+
+###########################################################################
+
+## read sample info
+
+sampleinfo=read.table(paste(pathTCGA, "all_info_paired_samples.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
+sampleinfo$case_id=as.character(sampleinfo$case_id)
+rownames(sampleinfo)=sampleinfo$id
+
+sampleinfo$sample_type[which(sampleinfo$sample_type=="Solid Tissue Normal")]="Non-Tumor"
+sampleinfo$sample_type[which(sampleinfo$sample_type=="Primary Tumor")]="Tumor"
+
+########################################################################
+
+save(list=c("tumor.samples", "liver.samples"), file=paste(pathRData, "data.sample.info.TCGA.RData",sep=""))
+
+###########################################################################
+
