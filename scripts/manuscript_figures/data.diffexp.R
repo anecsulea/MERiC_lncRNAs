b46043b9b88f859a58a2696246c4e9aac4c5738d
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2024-04-08 09:28:26 +0200

    load diffexp

--- scripts/manuscript_figures/data.diffexp.R
+++ scripts/manuscript_figures/data.diffexp.R
@@ -12,15 +12,14 @@ annot="AllTranscripts_Ensembl109_noMT_norRNA_nohaplo"
 
 diffexp.grades=read.table(paste(pathDifferentialExpression, annot, "/DifferentialExpression_EdmondsonGrade_34_vs_12.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
 
-diffexp.tissues=read.table(paste(pathDifferentialExpression, annot, "/DifferentialExpression_Tumor_vs_Liver.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
+diffexp.tnt.meric=read.table(paste(pathDifferentialExpression, annot, "/DifferentialExpression_Tumor_vs_NonTumor.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
 
 ## TCGA
 
-diffexp.tnt=read.table(paste(pathDifferentialExpressionTCGA, annot, "/DifferentialExpression_Tumor_vs_NonTumor.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
-rownames(diffexp.tnt)=diffexp.tnt$GeneID
+diffexp.tnt.tcga=read.table(paste(pathDifferentialExpressionTCGA, annot, "/DifferentialExpression_Tumor_vs_NonTumor.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
 
 ##########################################################################
 
-save(list=c("diffexp.grades", "diffexp.tissues", "diffexp.tnt"), file=paste(pathRData, "data.diffexp.RData",sep=""))
+save(list=c("diffexp.grades", "diffexp.tnt.meric", "diffexp.tnt.tcga"), file=paste(pathRData, "data.diffexp.RData",sep=""))
 
 ##########################################################################
